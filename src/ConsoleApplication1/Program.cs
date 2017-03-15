da887ce1395c9ede271c5154fa96ae348f01bb49
jarmo <jarmhab@users.noreply.github.com>
2017-03-15 08:53:15 +0200

    Added comic books controller

---
+++ src/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static String Name;
+        public static String LastName;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta palun oma eesnimi");
+            Name = Console.ReadLine();
+            Console.WriteLine("Sisesta palun oma perekonnanimi");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Hello" + " " + Name +" "+LastName+" and welcome to room 503!");
+        }
+    }
+}
